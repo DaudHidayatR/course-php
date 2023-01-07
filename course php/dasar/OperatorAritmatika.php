8f531bce2486f41bab6f065fca58e82cb16e9513
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 03:02:08 +0700

    belajar operasi aritmatika

---
+++ course php/dasar/OperatorAritmatika.php
@@ -0,0 +1,20 @@
+<?php
+
+use LDAP\Result;
+
+$a = 10;
+$b = 10;
+
+$result = $a + $b;
+var_dump($result);
+
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+$resultposititve = +$result;
+var_dump($resultposititve);
+
+$resultmodulo = 100 % 3;
+var_dump($resultmodulo);
+
+ 
