edac6067f1b131f4ee5d5e588cf0c2b4f7086460
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 02:01:50 +0700

    belajar type data string

---
+++ course php/dasar/TypeDataString.php
@@ -0,0 +1,15 @@
+<?php
+echo 'name :';
+echo 'daud Hidayat Ramadhan';
+echo "\n";
+
+echo "name : ";
+echo "daud\t Hidayat\t Ramadhan\n";
+
+echo <<<Daud
+ini adalah contoh dari string heredoc \n
+Daud;
+
+echo <<<'Daud'
+ini adalah contoh dari string nowdoc
+Daud;
