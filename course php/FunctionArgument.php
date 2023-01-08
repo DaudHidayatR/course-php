e32387c59ba126b60303d90980bea7a070585ae2
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 21:53:26 +0700

    belajar function Argument

--- course php/FunctionArgument.php
+++ course php/FunctionArgument.php
@@ -8,3 +8,20 @@ function hello($firstName, $lastName = "hidayat")
     echo "hello $firstName $lastName" . PHP_EOL;
 }
 hello("daud");
+
+function sum(int $first, int $last){
+    $total = $first + $last;
+    echo " total $first + $last = $total" . PHP_EOL;
+}
+sum(100, 100);
+
+function sumAll(...$values){
+    $total = 0;
+    foreach($values as $value){
+        $total += $value;
+    }
+    echo "Total ".implode(",", $values). " = $total".PHP_EOL;
+}
+$values = [1, 2, 3, 4, 5];
+sumALL(1, 2, 3, 4, 5);
+sumAll(...$values);
