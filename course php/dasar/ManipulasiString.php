fcc734aca4e8d770f462184852eb45fa1bfc4b21
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 08:28:22 +0700

    belajar Manipulasi String

---
+++ course php/dasar/ManipulasiString.php
@@ -0,0 +1,23 @@
+<?php 
+$name ="Daud HIdayat Ramadhan";
+
+echo "name : " . $name. PHP_EOL;
+echo "value : " . 1000 . PHP_EOL;
+
+$valueString= (string)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)"1.533";
+var_dump($valueFloat);
+
+$name= "daud";
+echo $name[0] . PHP_EOL;
+echo $name[1] . PHP_EOL;
+echo $name[2] . PHP_EOL;
+echo $name[3] . PHP_EOL;
+
+echo "hello , $name, Selamat Belajar php ". PHP_EOL;
+echo "hello , {$name}s, Selamat Belajar php ". PHP_EOL;
