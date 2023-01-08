55c6b7cd366ede42bc1a8b5382a534bd466a680c
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 12:03:23 +0700

    belajar Goto operator

---
+++ course php/dasar/GotoOperator.php
@@ -0,0 +1,17 @@
+<?php
+goto a;
+echo "hello world";
+
+a:
+echo "hello A";
+
+$count = 0;
+while (true) {
+    echo "imi adalah for while ke-$count" . PHP_EOL;
+    $count++;
+    if ($count > 10) {
+        goto end;
+    }
+}
+end:
+echo "endloop";
