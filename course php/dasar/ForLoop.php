5bcbde517b6283f6e3c0e1344227ff2105a29f5e
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-08 09:41:02 +0700

    belajar ForLoop

--- course php/dasar/ForLoop.php
+++ course php/dasar/ForLoop.php
@@ -6,3 +6,11 @@ for ($i = 0; $i <=10; $i++) {
 for ($i = 10; $i >1; $i--) {
     echo "Ini adalah for loop ke-$i". PHP_EOL;
 }
+
+for ($i = 0; $i <=10; $i++) :
+    echo "Ini adalah for loop ke-$i". PHP_EOL;
+endfor;
+
+for ($i = 10; $i >1; $i--) :
+    echo "Ini adalah for loop ke-$i". PHP_EOL;
+endfor;
