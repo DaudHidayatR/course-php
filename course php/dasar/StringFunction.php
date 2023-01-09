c74e34935e0b0941242ba187bebdfdcbea4ee9ec
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-09 14:41:34 +0700

    belajar String function

---
+++ course php/dasar/StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10, 11, 22, 33, 44, 55]));
+var_dump(explode(" ", "daud hidayat ramadhan"));
+var_dump(strtolower("daud hidayat ramadhan"));
+var_dump(strtoupper("daud hidayat ramad"));
+var_dump(trim("     daud        hidayat"));
+var_dump(substr("daud Hidayat ramadhan", 0, 3));
