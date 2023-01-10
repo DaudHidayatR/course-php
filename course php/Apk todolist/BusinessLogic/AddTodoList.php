a8fe3e210294e0cf794a99f48bfd6da2de385a0a
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-10 23:00:37 +0700

    membuat add todo list

--- course php/Apk todolist/BusinessLogic/AddTodoList.php
+++ course php/Apk todolist/BusinessLogic/AddTodoList.php
@@ -1,6 +1,8 @@
 <?php
 // menambah todolist
-function addTodolist()
+function addTodolist(string $todo)
 {
-
+    global $todoList;
+    $number = sizeof($todoList)+1;
+    $todoList[$number] = $todo;
 }
