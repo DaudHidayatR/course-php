4935e1e841b686a4a677ccdeedbff8b9e973713c
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-10 16:57:12 +0700

    menentukan view

--- course php/Apk todolist/app.php
+++ course php/Apk todolist/app.php
@@ -2,6 +2,8 @@
 require_once("model\TodoList.php");
 require_once("BusinessLogic\ShowTodoList.php");
 require_once("BusinessLogic\AddTodoList.php");
-require_once("BusinessLogic\menghapusTodoList.php");
-
+require_once("BusinessLogic\RemoveTodoList.php");
+require_once("view/viewAddTodoList.php");
+require_once("view/viewRemoveTodoList.php");
+require_once("view/viewShowTodoList.php");
 echo "Aplikasi Todolist" . PHP_EOL;
