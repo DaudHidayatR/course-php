4c406a0dad61fc08e6462d0668429c39a271bffe
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-10 19:40:34 +0700

    menampilkan Todolist

--- course php/Apk todolist/BusinessLogic/ShowTodoList.php
+++ course php/Apk todolist/BusinessLogic/ShowTodoList.php
@@ -2,5 +2,9 @@
 // menampilkan todolist
 function showTodoList()
 {
-
+    global $todoList;
+    echo "TODOLIST" . PHP_EOL;
+    foreach ($todoList as $number =>$value){
+        echo "$number .$value". PHP_EOL;
+    }
 }
