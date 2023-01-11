8807eadedf5ca24b5cc54dac9347813a448e5c96
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-12 00:48:56 +0700

    belajar OOP Constructor overriding

--- course php/OOP/data/manager.php
+++ course php/OOP/data/manager.php
@@ -2,13 +2,24 @@
 class manager
 {
     var string $name;
+    var string $title;
+
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
 
     function sayHello(string $name): void
     {
-        echo "Hi $name , my name is $this->name". PHP_EOL;
+        echo "Hi $name , my name is $this->name" . PHP_EOL;
     }
 }
-class vicePresident extends manager {
+class vicePresident extends manager
+{
+    public function __construct (string $name = ""){
+        parent::__construct($name, "Vp");
+    }
     function sayHello(string $name): void
     {
         echo "Hi $name , my name is VP $this->name" . PHP_EOL;
