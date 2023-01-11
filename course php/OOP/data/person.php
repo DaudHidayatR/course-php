dd341f19a7d45201a5f7c4e4462112e254b8077d
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-11 11:32:05 +0700

    belajar OOP function

--- course php/OOP/data/person.php
+++ course php/OOP/data/person.php
@@ -4,4 +4,7 @@ class person
     var string $name;
     var ?string $address = null;
     var string $country = "indonesia";
+    function sayHello(string $name){
+        echo "Hello $name"  . PHP_EOL;
+    }
 }
---
+++ course php/OOP/function.php
@@ -0,0 +1,5 @@
+<?php
+require_once "data/person.php";
+$person1 = new Person();
+
+$person1->sayHello("daud");
