9cef15992adc97c93c6bb3b86ef3a0fd289ddf36
Daud Hidayat Ramadhan <daud28ramadhan@gmail.com>
2023-01-11 19:18:42 +0700

    belajar OOP inheritance

---
+++ course php/OOP/data/manager.php
@@ -0,0 +1,13 @@
+<?php
+class manager
+{
+    var string $name;
+
+    function sayHello(string $name): void
+    {
+        echo "Hi $name , my name is $this->name". PHP_EOL;
+    }
+}
+class vicePresident extends manager {
+
+}
