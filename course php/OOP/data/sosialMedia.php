018c52465d0327be9a0144bc8075bd2a5ec6aba9
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-13 22:13:50 +0700

    belajar OOP final function

--- course php/OOP/data/sosialMedia.php
+++ course php/OOP/data/sosialMedia.php
@@ -1,9 +1,14 @@
-<?php 
-class sosialMedia{
+<?php
+class sosialMedia
+{
     public string $name;
 }
-final class  facebook extends sosialMedia{
-    
+final class facebook extends sosialMedia
+{
+    final public function login(string $usename, string $password): bool
+    {
+        return true;
+    }
 }
 
 ?>
