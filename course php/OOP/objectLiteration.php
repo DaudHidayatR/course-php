8ccf4bcc07a33bb39b6c25c91dc8ec606a632f7f
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-15 01:00:08 +0700

    Belajar OOP Generator

--- course php/OOP/objectLiteration.php
+++ course php/OOP/objectLiteration.php
@@ -1,21 +1,31 @@
 <?php
-class data implements IteratorAggregate {
+class data implements IteratorAggregate
+{
     var string $first = "first";
     public string $second = "second";
     private string $third = "third";
     protected string $fourth = "fourth";
-    public function getIterator()
+    // public function getIterator()
+    // {
+    //     $array = [
+    //         "first" => $this->first,
+    //         "second" => $this->second,
+    //         "third" => $this->third,
+    //         "fourth" => $this->fourth,
+    //     ];
+    //     return  new ArrayIterator($array);
+    // }
+    public function getIterator(): Iterator
     {
-        $array = [
-            "first" => $this->first,
-            "second" => $this->second,
-            "third" => $this->third,
-            "fourth" => $this->fourth,
-        ];
-        return  new ArrayIterator($array);
+
+        yield "first" => $this->first;
+        yield "second" => $this->second;
+        yield "third" => $this->third;
+        yield "fourth" => $this->fourth;
+
     }
 }
 $data = new Data();
-foreach($data as $key => $value){
-    echo "$key : $value".PHP_EOL;
+foreach ($data as $key => $value) {
+    echo "$key : $value" . PHP_EOL;
 }
