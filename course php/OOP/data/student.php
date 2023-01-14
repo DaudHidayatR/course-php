0edcb287c269b82d08d2fce3afc8d1951450c2a2
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-15 02:35:17 +0700

    Belajar OOP magic Method invoke, toString, debugInfo

--- course php/OOP/data/student.php
+++ course php/OOP/data/student.php
@@ -1,5 +1,6 @@
 <?php
-class students{
+class students
+{
     public string $name;
     public string $id;
     public string $address;
@@ -16,5 +17,26 @@ class students{
     public function __clone()
     {
         unset($this->sample);
+    }
+    public function __toString(): string
+    {
+        return "Student id: $this->id,  name: $this->name, address: $this->address";
+    }
+
+    public function __invoke(... $arguments):void
+    {
+        $join = join(",", $arguments);
+        echo "invoke student with arguments $join".PHP_EOL;
+    }
+    public function __debugInfo()
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "address" => $this->address,
+            "sample" => $this->sample,
+            "AUTHOR"=>"DAUD",
+            "version" => "1.0",
+        ];
     }
 }
---
+++ course php/OOP/invoke.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . "/data/student.php";
+
+$students1 = new students();
+$students1->name = "daud";
+$students1->id = "1";
+$students1->address = "jogja";
+$students1->setSample("hello");
+$students1();
