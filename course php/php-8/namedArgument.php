6b13a969e51b98d2fce28e8c24bebb9e260c3ada
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-16 15:47:36 +0700

    belajar PHP v8.1 name Argument

---
+++ course php/php-8/namedArgument.php
@@ -0,0 +1,10 @@
+<?php
+function sayHello(string $first, string $middle =" ", string $last):void{
+    echo "hellp $first $middle $last".PHP_EOL;
+
+}
+SayHello("daud","hidayat","ramadhan");
+
+sayHello(last: "daud",first:"hidayat",middle:"ramadhan");
+
+// SayHello(first: "daud", last: "hidayat");
