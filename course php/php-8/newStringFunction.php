af0f780edb4d01cc271a2d4a5e7c047cae05a674
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-17 23:02:55 +0700

    belajar PHP v8.1 new string function

---
+++ course php/php-8/newStringFunction.php
@@ -0,0 +1,12 @@
+<?php
+var_dump(str_contains("daud hidayat", "daud"));
+var_dump(str_contains("daud hidayat", "hidayat"));
+var_dump(str_contains("daud hidayat", "ramadhan"));
+
+var_dump(str_starts_with("daud hidayat", "daud"));
+var_dump(str_starts_with("daud hidayat", "daud"));
+var_dump(str_starts_with("daud hidayat", "daud"));
+
+var_dump(str_ends_with("daud hidayat", "daud"));
+var_dump(str_ends_with("daud hidayat", "daud"));
+var_dump(str_ends_with("daud hidayat", "daud"));
