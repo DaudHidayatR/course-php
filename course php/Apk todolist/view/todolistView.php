1c373567f6bf8af2a9f95e12a3153757602458dd
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-18 23:04:47 +0700

    membuat view  todolist

---
+++ course php/Apk todolist/view/todolistView.php
@@ -0,0 +1,16 @@
+<?php
+namespace View{
+    class TodoLIstView
+    {
+        function showTodolist(): void
+        {
+
+        }function addTodolist(): void{
+
+        }
+        function removeTodolist(): void
+        {
+            
+        }
+    }
+}
