ab86b21885b2050cbb034d8068aa1c0bdb491f2a
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-01-19 17:48:58 +0700

    membuat view menampilkan todolist

--- course php/Apk todolist/view/todolistView.php
+++ course php/Apk todolist/view/todolistView.php
@@ -1,10 +1,32 @@
 <?php
 namespace View{
+use Entity\Todolist;
+use Service\TodolistService;
+    use Helper\InputHelper;
     class TodoLIstView
     {
+        private TodolistService $todolistService;
         function showTodolist(): void
         {
+            while (true) {
+                $this->todolistService->showTodoList();
+                echo "MENU" . PHP_EOL;
+                echo "1. Tambah Todo" . PHP_EOL;
+                echo "2. Hapus Todo" . PHP_EOL;
+                echo "x. keluar" . PHP_EOL;
 
+                $pilihan = InputHelper::input("pilih");
+                if ($pilihan == "1") {
+                    $this->addTodolist();
+                } else if ($pilihan == "2") {
+                    $this->removeTodolist();
+                } else if ($pilihan == "x") {
+                    break;
+                } else {
+                    echo "pilihan anda salah, silahkan masukaan ulang" . PHP_EOL;
+                }
+            }
+            echo "sampai jumpa lagi" . PHP_EOL;
         }function addTodolist(): void{
 
         }
