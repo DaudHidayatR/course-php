ca551cd9940a36443a23e6a561413e19f96351e2
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-06 22:22:04 +0700

    Test Seluruh Aplikasi

--- course php/Apk todolist/app.php
+++ course php/Apk todolist/app.php
@@ -4,14 +4,17 @@ require_once __DIR__ . ("/helper/inputHelper.php");
 require_once __DIR__ . ("/repository/todolistRepository.php");
 require_once __DIR__ . ("/Service/todolistService.php");
 require_once __DIR__ . ("/view/todoListView.php");
+require_once __DIR__ . ("/config/database.php");
 
 use Entity\Todolist;
 use helper\InputHelper;
 use Repository\TodolistRepositorImpl;
 use Service\TodolistServiceImpl;
 use View\TodoLIstView;
+
 echo "Aplikasi Todolist" . PHP_EOL;
-$todolistRepository = new TodolistRepositorImpl();
+$conn = \config\Database::getConnection();
+$todolistRepository = new TodolistRepositorImpl($conn);
 $todolistService = new TodolistServiceImpl($todolistRepository);
 $todolistView = new TodoListView($todolistService);
 
