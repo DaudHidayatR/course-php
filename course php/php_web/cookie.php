15af8854d55b51c4f4c9f874b919708048dff8e7
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-09 23:57:07 +0700

    Belajar Cookie

---
+++ course php/php_web/cookie.php
@@ -0,0 +1,3 @@
+<?php
+setcookie('X-Belajar-cookie','Daud Hidayat Ramadhan');
+header('location: showCookie.php');
---
+++ course php/php_web/showCookie.php
@@ -0,0 +1,3 @@
+<?php
+$belajarCookie = $_COOKIE['X-Belajar-cookie'];
+echo $belajarCookie;
