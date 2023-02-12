5f9dd0ccff4bdfc54c00058123294e16a9b0694f
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-12 21:25:39 +0700

    Belajar Upgrade versi Library

--- course php/belajar-php-composer/helloLibrary.php
+++ course php/belajar-php-composer/helloLibrary.php
@@ -3,4 +3,5 @@ require_once __DIR__ . ('/vendor/autoload.php');
 
 $customer = new \Daudhidayatramadhan\Belajar\Customers("daud");
 
+echo $customer->sayHello().PHP_EOL;
 echo $customer->sayHello("siraj").PHP_EOL;
