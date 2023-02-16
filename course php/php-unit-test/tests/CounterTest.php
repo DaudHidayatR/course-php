280ea0b80da7c456ca48aa034fb762fe523aed77
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-16 19:50:57 +0700

    Belajar Skip Test

--- course php/php-unit-test/tests/CounterTest.php
+++ course php/php-unit-test/tests/CounterTest.php
@@ -29,12 +29,12 @@ Class CounterTest extends TestCase
      * @test
      */
     public function  increment(){
+        self::markTestSkipped("Masih ada bug");
         $this->counter->increment();
         $this->assertEquals(1, $this->counter->getCounter());
     }
     public function testFirst():Counter
     {
-
         $this->counter->increment();
         $this->assertEquals(1, $this->counter->getCounter());
         return $this->counter;
@@ -58,5 +58,19 @@ Class CounterTest extends TestCase
     protected function after(): void
     {
         echo "After" . PHP_EOL;
+    }
+
+    /**
+     * @requires OSFAMILY Linux
+     */
+    public function testOnlyLinux(){
+        self::assertTrue(true, 'only in linux');
+    }
+    /**
+     * @requires  PHP >= 8
+     * @requires  OSFAMILY Windows
+     */
+    public function testOnlyForWindows(){
+        self::assertTrue(true, "ony for Windows and PHP 8");
     }
 }
