cf10fa39230b7b335a996d92ee2aede32aa167e1
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-18 23:02:16 +0700

    Belajar Router Sederhana

--- course php/belajar-php-mvc/public/index.php
+++ course php/belajar-php-mvc/public/index.php
@@ -3,4 +3,4 @@ $path = '/index';
 if ((isset($_SERVER['PATH_INFO']))){
     $path = $_SERVER['PATH_INFO'];
 }
-require  __DIR__.'/../app/view'. $path .'.php';
+require  __DIR__ . '/../app/View' . $path .'.php';
