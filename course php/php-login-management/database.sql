018d026e44c639f0ef5e16ba6fb8cfc33d546a1a
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-21 20:44:18 +0700

    Setup Database

---
+++ course php/php-login-management/database.sql
@@ -0,0 +1,20 @@
+create database php_login_management;
+
+create database php_login_management_test;
+
+create table users(
+    id VARCHAR(255)PRIMARY KEY ,
+    name VARCHAR(255) NOT NULL ,
+    password VARCHAR(255) NOT NULL
+) ENGINE InnoDB;
+
+CREATE TABLE sessions(
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL
+)ENGINE InnoDB;
+
+ALTER TABLE sessions
+ADD CONSTRAINT fk_sessions_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id);
+
