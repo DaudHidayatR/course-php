480c67b49841c47042f0c5767fd87bea3e0333cf
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-24 23:25:17 +0700

    Session Service

---
+++ course php/php-login-management/app/Service/SessionService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Daudhidayatramadhan\LoginManagement\Service;
+
+use Daudhidayatramadhan\LoginManagement\Domain\Session;
+use Daudhidayatramadhan\LoginManagement\Domain\User;
+use Daudhidayatramadhan\LoginManagement\Repository\SessionRepository;
+use Daudhidayatramadhan\LoginManagement\Repository\UserRepository;
+
+class SessionService
+{
+    public static string $COOKIE_NAME = "X-DHR-SESSION";
+    private SessionRepository $sessionRepository;
+    private UserRepository $userRepository;
+
+    public function __construct(SessionRepository $sessionRepository, UserRepository $userRepository)
+    {
+        $this->sessionRepository = $sessionRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    public function create(string $userId): Session
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->userId = $userId;
+
+        $this->sessionRepository->save($session);
+        setcookie(self::$COOKIE_NAME, $session->userId, time()+(60*60*24*30),"/" );
+        return $session;
+    }
+    public function destroy()
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? '';
+        $this->sessionRepository->deleteById($sessionId);
+        setcookie(self::$COOKIE_NAME, '', 1,"/" );
+    }
+    public function current(): ?User
+    {
+        $sessionId = $_COOKIE[self::$COOKIE_NAME] ?? '';
+
+        $session = $this->sessionRepository->findById($sessionId);
+        if($session == null ){
+            return null;
+        }
+        return $user = $this->userRepository->findById($sessionId->userId);
+    }
+}
