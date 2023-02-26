806bbeec4e8009c0a70d43a585584715a31feccf
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-26 18:43:35 +0700

    Unit Test User Profile Update Repository

--- course php/php-login-management/tests/Repository/UserRepositoryTest.php
+++ course php/php-login-management/tests/Repository/UserRepositoryTest.php
@@ -9,10 +9,13 @@ use PHPUnit\Framework\TestCase;
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $repository;
+    private  SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
         $this->repository = new  UserRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
         $this->repository->deleteAll();
 
     }
@@ -35,5 +38,23 @@ class UserRepositoryTest extends TestCase
     {
         $user = $this->repository->findById("notFound");
         self::assertNull($user);
+    }
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->id = '1';
+        $user->name = 'daud';
+        $user->password = 'daudhidayatramadhan';
+
+        $this->repository->save($user);
+        $user->name = "budi";
+        $this->repository->update($user);
+
+        $result = $this->repository->findById($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
+
     }
 }
